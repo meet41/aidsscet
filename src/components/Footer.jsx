612d02bfd2e38c15de361d65d175f222333c974e
Meet Patel <89350078+meet41@users.noreply.github.com>
2025-06-21 14:01:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Designed by Meet Patel - I.T. Batch 2023-26 <br /> & Maintained By AIDS Dept., SCET, Surat</p>
+      <p>Designed by Meet Patel - I.T. Batch 2023-26 & Maintained By AIDS Dept., SCET, Surat</p>
     </footer>
   );
 };
